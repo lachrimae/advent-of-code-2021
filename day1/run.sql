b84acf00be26b014a45b5e6108afc21f6288b9c4
Curran McConnell <curran.mcconnell@protonmail.com>
2021-12-05 01:19:52 -0500

    try out round 2

--- day1/run.sql
+++ day1/run.sql
@@ -1,4 +1,13 @@
 .load ./csv
 create virtual table t using csv(filename='numbered.txt');
-select * from t;
+select count(t2.c0)
+from t t1
+join t t2
+on t2.c0 - 1 = t1.c0
+where t2.c1 > t1.c1;
 
+select count(t4.c0)
+from t t1
+join t t4
+on t4.c0 - 3 = t1.c0
+where t4.c1 > t1.c1;
