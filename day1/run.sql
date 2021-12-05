a59ba13e2fd04d2e67458ccf3c32c042e3b56e52
Curran McConnell <curran.mcconnell@protonmail.com>
2021-12-05 16:25:22 -0500

    rename index to time

--- day1/run.sql
+++ day1/run.sql
@@ -1,14 +1,14 @@
 .load ./csv
 create virtual table t using csv(header=true, filename='numbered.txt');
 
-select count(t1."index")
+select count(t1.time)
 from t t1
 join t t2
-on t2."index" - 1 = t1."index"
+on t2.time - 1 = t1.time
 where t2.depth > t1.depth;
 
-select count(t1."index")
+select count(t1.time)
 from t t1
 join t t4
-on t4."index" - 3 = t1."index"
+on t4.time - 3 = t1.time
 where t4.depth > t1.depth;
