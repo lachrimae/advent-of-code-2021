9b9469d0eec3b54aa9c5d8c235206a1d2e5e954e
Curran McConnell <curran.mcconnell@protonmail.com>
2022-01-02 16:53:52 -0500

    solve part2

--- day5/Main.hs
+++ day5/Main.hs
@@ -12,6 +12,8 @@ data Edge = Edge Coordinate Coordinate
   deriving (Eq, Ord, Show)
 data Orientation = Vertical | Horizontal | Diagonal
   deriving Eq
+data Relation = Positive | Negative
+  deriving Eq
 
 orientation :: Edge -> Orientation
 orientation (Edge start end)
@@ -19,24 +21,51 @@ orientation (Edge start end)
   | projectY start == projectY end = Horizontal
   | otherwise                      = Diagonal
 
-points :: Edge -> [Coordinate]
-points edge@(Edge start end) =
-  case orientation edge of
-    Vertical ->
-      let high = projectY start `max` projectY end
-          low = projectY start `min` projectY end
-          raw = [(projectX start, y) | y <- [low..high]]
-       in fmap (uncurry Coordinate) raw
-    Horizontal ->
-      let high = projectX start `max` projectX end
-          low = projectX start `min` projectX end
-          raw = [(x, projectY start) | x <- [low..high]]
-       in fmap (uncurry Coordinate) raw
-    Diagonal -> []
+points1 :: Edge -> [Coordinate]
+points1 edge@(Edge start end) =
+  fmap (uncurry Coordinate) $
+    case orientation edge of
+      Vertical ->
+        let high = projectY start `max` projectY end
+            low = projectY start `min` projectY end
+         in [(projectX start, y) | y <- [low..high]]
+      Horizontal ->
+        let high = projectX start `max` projectX end
+            low = projectX start `min` projectX end
+         in [(x, projectY start) | x <- [low..high]]
+      Diagonal ->
+        []
 
-getPointsWithTwoOverlaps :: [Edge] -> Set Coordinate
-getPointsWithTwoOverlaps [] = empty
-getPointsWithTwoOverlaps as = getPointsWithTwoOverlaps' as empty empty
+points2 :: Edge -> [Coordinate]
+points2 edge@(Edge start end) =
+  fmap (uncurry Coordinate) $
+    case orientation edge of
+      Vertical ->
+        let high = projectY start `max` projectY end
+            low = projectY start `min` projectY end
+         in [(projectX start, y) | y <- [low..high]]
+      Horizontal ->
+        let high = projectX start `max` projectX end
+            low = projectX start `min` projectX end
+         in [(x, projectY start) | x <- [low..high]]
+      Diagonal ->
+        let xDelta = (projectX end - projectX start)
+            yDelta = (projectY end - projectY start)
+            relation = if xDelta * yDelta >= 0 then Positive else Negative
+            highX = projectX start `max` projectX end
+            highY = projectY start `max` projectY end
+            lowX = projectX start `min` projectX end
+            lowY = projectY start `min` projectY end
+            ys = [lowY..highY]
+            orderedYs =
+              case relation of
+                Positive -> ys
+                Negative -> reverse ys
+         in zip [lowX..highX] orderedYs
+
+getPointsWithTwoOverlaps :: (Edge -> [Coordinate]) -> [Edge] -> Set Coordinate
+getPointsWithTwoOverlaps _ [] = empty
+getPointsWithTwoOverlaps points as = getPointsWithTwoOverlaps' as empty empty
   where
     getPointsWithTwoOverlaps' [] _ pointsWithTwoOverlaps = pointsWithTwoOverlaps
     getPointsWithTwoOverlaps' (edge : edges) pointsWithOneOverlap pointsWithTwoOverlaps =
@@ -75,5 +104,7 @@ main = do
       Done _ res -> pure res
       x -> error $ show x
     x -> error $ show x
-  let overlappedEdges = getPointsWithTwoOverlaps edges
-  putStrLn $ show $ size $ overlappedEdges
+  let overlappedEdges1 = getPointsWithTwoOverlaps points1 edges
+  let overlappedEdges2 = getPointsWithTwoOverlaps points2 edges
+  putStrLn $ show $ size $ overlappedEdges1
+  putStrLn $ show $ size $ overlappedEdges2
