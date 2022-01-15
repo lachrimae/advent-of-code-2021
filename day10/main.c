5a3f888a1229d720b6b62bb08a4ede9123ee7867
Curran McConnell <curran.mcconnell@protonmail.com>
2022-01-14 19:00:17 -0500

    complete day10 part1

--- day10/main.c
+++ day10/main.c
@@ -1,18 +1,27 @@
 #include <stddef.h>
 #include <stdlib.h>
 #include <stdio.h>
+#define MAX_STACK_SIZE 128
 
-char stack[128];
+// one global stack region which can be reused over and over again
+char stack[MAX_STACK_SIZE];
 
 void push(size_t* index, char val) {
+  if (*index >= MAX_STACK_SIZE) {
+    perror("ran out of stack space");
+    exit(1);
+  }
   stack[*index] = val;
   *index += 1;
   return;
 }
 
 char pop(size_t *index) {
-  char res = stack[*index];
+  if (*index < 1) {
+    return -1;
+  }
   *index -= 1;
+  char res = stack[*index];
   return res;
 }
 
@@ -54,10 +63,12 @@ int error_value(char brace) {
   }
 }
 
-int check_line_for_error(char* line) {
+int check_line_for_error(char* cursor, ssize_t line_len) {
   size_t index = 0;
-  char c;
-  while(c = *line) {
+  // the final character on the cursor will be '\n', so we
+  // subtract one from line_len because we don't care about it
+  for (int i = 0; i < line_len - 1; i += 1) {
+    char c = cursor[i];
     if (is_open_brace(c)) {
       push(&index, c);
     }
@@ -67,7 +78,6 @@ int check_line_for_error(char* line) {
         return error_value(c);
       }
     }
-    line += 1;
   }
   return 0;
 }
@@ -78,29 +88,30 @@ int main(int argc, char* argv[]) {
     exit(1);
   }
 
-  FILE *f = fopen(argv[1], "w");
+  char *line = NULL;
+  size_t len = 0;
+  ssize_t nread;
 
-  char *buffer;
-  size_t bufsize = 32;
-  size_t characters;
-
-  buffer = (char*)malloc(bufsize * sizeof(char));
-  if (buffer == NULL) {
-    perror("couldn't allocate buffer");
+  FILE *stream = fopen(argv[1], "r");
+  if (stream == NULL) {
+    perror("fopen");
     exit(1);
   }
 
-  int result = 0;
-  while(1) {
-    characters = getline(&buffer, &bufsize, f);
-    if (characters == -1) {
-      break;
-    }
-    buffer[characters] = '\0';
+  int result1 = 0;
+  // getline implicitly calls malloc when line = NULL.
+  // This could be made faster by preallocating
+  // one buffer that is sufficiently large to load every
+  // line into it.
+  while ((nread = getline(&line, &len, stream)) != -1) {
+    int new_res = check_line_for_error(line, nread);
+    result1 += new_res;
 
-    result += check_line_for_error(buffer);
+    free(line);
+    line = NULL;
   }
+  fclose(stream);
 
-  printf("result = %d", result);
-  return 0;
+  printf("result1 = %d\n", result1);
+  exit(0);
 }
