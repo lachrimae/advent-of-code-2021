d7514fe0aca4fb3b10fba4df90d9499581299d20
Curran McConnell <curran.mcconnell@protonmail.com>
2022-01-15 14:15:42 -0500

    complete day10

--- day10/main.c
+++ day10/main.c
@@ -63,12 +63,12 @@ int error_value(char brace) {
   }
 }
 
-int check_line_for_error(char* cursor, ssize_t line_len) {
+int check_line_for_error(char* line, ssize_t line_len, size_t *final_stack_index) {
   size_t index = 0;
-  // the final character on the cursor will be '\n', so we
+  // the final character on the line will be '\n', so we
   // subtract one from line_len because we don't care about it
-  for (int i = 0; i < line_len - 1; i += 1) {
-    char c = cursor[i];
+  for (int line_cursor = 0; line_cursor < line_len - 1; line_cursor += 1) {
+    char c = line[line_cursor];
     if (is_open_brace(c)) {
       push(&index, c);
     }
@@ -79,7 +79,40 @@ int check_line_for_error(char* cursor, ssize_t line_len) {
       }
     }
   }
+  *final_stack_index = index;
   return 0;
+}
+
+long long int score_autocomplete(size_t stack_index) {
+  char left_brace;
+  long long int score = 0;
+  while ((left_brace = pop(&stack_index)) != -1) {
+    score *= 5;
+    switch (left_brace) {
+      case '(':
+        score += 1;
+	break;
+      case '[':
+        score += 2;
+	break;
+      case '{':
+	score += 3;
+	break;
+      case '<':
+	score += 4;
+	break;
+      default:
+	break;
+    }
+  }
+  return score;
+}
+
+// more or less copy-pasted from http://www.cplusplus.com/reference/cstdlib/qsort/
+int cmp(const void* a, const void* b) {
+  if ( *(long long int*)a <  *(long long int*) b) return -1;
+  if ( *(long long int*)a == *(long long int*) b) return 0;
+  if ( *(long long int*)a >  *(long long int*) b) return 1;
 }
 
 int main(int argc, char* argv[]) {
@@ -99,19 +132,34 @@ int main(int argc, char* argv[]) {
   }
 
   int result1 = 0;
+  long long int result2 = 0;
+  size_t num_result2s = 0;
+  long long int result2s[90];
   // getline implicitly calls malloc when line = NULL.
   // This could be made faster by preallocating
   // one buffer that is sufficiently large to load every
   // line into it.
   while ((nread = getline(&line, &len, stream)) != -1) {
-    int new_res = check_line_for_error(line, nread);
-    result1 += new_res;
+    size_t stack_index;
+    int new_res = check_line_for_error(line, nread, &stack_index);
+
+    if (new_res == 0) {
+      result2s[num_result2s] = score_autocomplete(stack_index);
+      num_result2s += 1;
+    } else {
+      result1 += new_res;
+    }
 
     free(line);
     line = NULL;
   }
   fclose(stream);
 
+  qsort(result2s, num_result2s, sizeof(long long int), cmp);
+  size_t median_index = num_result2s / 2;
+  result2 = result2s[median_index];
+
   printf("result1 = %d\n", result1);
+  printf("result2 = %lld\n", result2);
   exit(0);
 }
